0b6d42631cfbf7e120c80c6e9188d08f9ad1e544
Aniket Gholve <aniketgholve02@gmail.com>
2023-12-29 20:46:35 +0530

    changed image path

--- src/components/MocktestComp/Mockcard.jsx
+++ src/components/MocktestComp/Mockcard.jsx
@@ -2,11 +2,14 @@
 import "./style/mock.css"
 
 const Mockcard = ({ practice, name, date, participants, duration, img }) => {
+  const buyTest=()=>{
+    
+  }
   return (
     <div className={practice === "topic" ? "practiceCard" : practice === "company" ? "companyCard" : "mockCard"}>
       {!practice && <div className="mockOptions">
-        <img src="/public/info.png" alt="" />
-        <img src="/public/share.svg" alt="" />
+        <img src="/info.png" alt="" />
+        <img src="/share.svg" alt="" />
       </div>}
       <div className='mockCardDiv'>
         <img src={img ?? "https://prepbytes-misc-images.s3.ap-south-1.amazonaws.com/assets/1663504995662-Aptitude%20Based%20Test%203%20icon.svg"} alt="" />
@@ -29,7 +32,7 @@ const Mockcard = ({ practice, name, date, participants, duration, img }) => {
         </div>
       </div>}
       <div className="btn-div" >
-        <button className="btn btn-test">Test Now</button>
+        <button className="btn btn-test" onClick={()=>buyTest(name)}>Test Now</button>
       </div>
     </div>
   )
